a7403f635bc1e47a2d2ba6e9e7634223ac4ff246
JDhillon24 <dhillonjagdeep13@gmail.com>
2025-01-30 16:30:08 -0800

    working on incorporating socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 require("./config/db");
-
+const http = require("http");
+const { Server } = require("socket.io");
 const app = require("express")();
 const cors = require("cors");
+
+const server = http.createServer(app);
 
 app.use(
   cors({
@@ -21,8 +24,34 @@ app.use(cookieParser());
 app.use(bodyParser());
 app.use(urlEncoded);
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("sendMessage", (message) => {
+    console.log("Received:", message);
+    io.emit("receiveMessage", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
 app.use("/user", UserRouter);
 
 // app.use(routes);
 
-module.exports = app;
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
--- frontend/src/Routes/Home.tsx
+++ frontend/src/Routes/Home.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import useAuth from "../hooks/useAuth";
 import useLogout from "../hooks/useLogout";
+import { io, Socket } from "socket.io-client";
 
 const Home = () => {
   const logout = useLogout();
+  const [socket, setSocket] = useState<Socket | null>(null);
+
+  useEffect(() => {
+    const newSocket = io("http://localhost:5000", {
+      withCredentials: true,
+      transports: ["websocket"],
+    });
+
+    setSocket(newSocket);
+
+    newSocket.on("connect", () => {
+      console.log("Connected to server:", newSocket.id);
+    });
+
+    newSocket.on("connect_error", (err) => {
+      console.error("Connection failed:", err);
+    });
+
+    newSocket.on("disconnect", (reason) => {
+      console.log("Disconnected:", reason); // Connection lost details
+    });
+
+    // Cleanup function to prevent duplicate connections
+    return () => {
+      newSocket.disconnect();
+      console.log("socket disconnected on unmount");
+    };
+  }, []);
 
   const signOut = async () => {
     await logout();
