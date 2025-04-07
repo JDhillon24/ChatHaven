853c47a25c9c5e2141f6fc60d36d50f3452d2ff3
JDhillon24 <dhillonjagdeep13@gmail.com>
2025-04-07 16:07:31 -0700

    Update axios.tsx

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL: string = "http://localhost:5000";
+const BASE_URL: string = "https://chathaven.railway.app";
 
 export default axios.create({
   baseURL: BASE_URL,
