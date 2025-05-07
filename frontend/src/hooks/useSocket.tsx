2cc1b598bca4e44d7cf9d84915789cdfbb024878
JDhillon24 <dhillonjagdeep13@gmail.com>
2025-05-07 15:11:20 -0700

    Update useSocket.tsx

--- frontend/src/hooks/useSocket.tsx
+++ frontend/src/hooks/useSocket.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { io, Socket } from "socket.io-client";
 import useAuth from "./useAuth";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL;
+// const SOCKET_URL = import.meta.env.VITE_API_URL;
 
 const useSocket = () => {
   //ref for current socket instance
